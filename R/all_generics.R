69495531857930d74a8150b5e3a06ed65a5a0610
Rui Guan <guan@dell-node-13.local>
2019-04-26 18:52:29 +0200

    add adj.R

--- R/all_generics.R
+++ R/all_generics.R
@@ -15,14 +15,30 @@ setGeneric("fit_tabs", function(x) {
 
 ###############################################################################
 
+#' Normalize the @tab and obtain @norm for later analysis.
 #' @param x The input mina object with quantitative tab / a matrix needed to be
 #' normalized.
-#'
 #' @param method The method used for the normalization of quantitative table.
 #' @examples
-#' norm_tab(x, method = "total")
+#' x <- norm_tab(x, method = "total")
 #' @export
 
 setGeneric("norm_tab", function(x, method, depth = 1000, replace = TRUE) {
     standardGeneric("norm_tab")
+})
+
+###############################################################################
+
+#' Calculate the adjacacency matrix of @norm by correlation.
+#' @param x An object of the class mina with @norm defined or any quantitative
+#' matrix.
+#' @param method The correlation coeffient used for adjacacency matrix.
+#' @param threads (optional) The number of threads used for parallel running.
+#' @param nblocks (optional) The number of row / column for splitted sub-matrix.
+#' @examples
+#' x <- adj(x, method = "pearson")
+#' @export
+
+setGeneric("adj", function(x, method, threads = 80, nblocks = 400) {
+    standardGeneric("adj")
 })
